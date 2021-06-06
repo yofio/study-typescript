62c2fffca95f6656c122687634ff07597ffe73b8
yofio <d.y215jump@i.softbank.jp>
2021-06-06 15:14:12 +0900

    number型、string型、array型の勉強

---
+++ src/array.ts
@@ -0,0 +1,14 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1, 2, 3];
+
+let strings: string[] = ["大阪", "東京", "名古屋"];
+
+let nijigenhairetu: number[][] = [
+  [50, 100],
+  [150, 300],
+];
+
+let hairtu: (string | number | boolean)[] = [1, false, "japan"];
---
+++ src/number-string.ts
@@ -0,0 +1,12 @@
+export {};
+
+let year: number = 11;
+// year = false;
+console.log(year);
+
+let age: number = 11;
+console.log(age);
+
+let name: string = "aia";
+// name = 10;
+console.log(name);
