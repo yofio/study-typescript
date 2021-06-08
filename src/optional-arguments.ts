ef4316ad9f916d6356dfdbb6b576c5e428adcbdf
yofio <d.y215jump@i.softbank.jp>
2021-06-08 16:09:45 +0900

    オプショナルなオプショナルなパラメーターの定義

---
+++ src/optional-arguments.ts
@@ -0,0 +1,15 @@
+export {};
+
+let bmi: (height: number, weight: number, printable?: boolean) => number = (
+  height: number,
+  weight: number,
+  printable?: boolean
+): number => {
+  const bmi: number = weight / (height * height);
+  if (printable) {
+    console.log({ bmi });
+  }
+  return bmi;
+};
+
+bmi(1.6, 48, true);
