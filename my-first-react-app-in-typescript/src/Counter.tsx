962751027b55073e478ecfca6cb83c428ed0edef
yofio <d.y215jump@i.softbank.jp>
2021-06-22 14:32:17 +0900

    useStateに型定義をしてみた

---
+++ my-first-react-app-in-typescript/src/Counter.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const Counter: React.FC = () => {
+  const [value, setValue] = useState<number>(0);
+
+  const increment = () => {
+    setValue((prevState) => prevState + 1);
+  };
+
+  const decrement = () => {
+    setValue((prevState) => prevState - 1);
+  };
+
+  return (
+    <div>
+      <div>value: {value}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+    </div>
+  );
+};
+
+export default Counter;
