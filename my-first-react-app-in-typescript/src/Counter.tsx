d04d9fa647cee15c2aabb8714a2bbab31df164d9
yofio <d.y215jump@i.softbank.jp>
2021-06-22 15:13:12 +0900

    useRefの少し応用

--- my-first-react-app-in-typescript/src/Counter.tsx
+++ my-first-react-app-in-typescript/src/Counter.tsx
@@ -16,12 +16,22 @@ const Counter: React.FC = () => {
     renderTimes.current += 1;
   });
 
+  const ref = useRef<HTMLInputElement>(null!);
+
+  const focusInput = () => {
+    // const current = ref.current;
+    // if (current != null) current.focus();
+    ref.current.focus();
+  };
+
   return (
     <div>
       <div>value: {value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
       <div>this component was re-rendered {renderTimes.current} times</div>
+      <input ref={ref} type="text" />
+      <button onClick={focusInput}>Click Me!</button>
     </div>
   );
 };
